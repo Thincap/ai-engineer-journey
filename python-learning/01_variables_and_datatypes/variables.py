49ed6622b30754d654edceda853a888bebc852c5
Parikshit <Sparikshit2001@gmail.com>
2026-07-19 12:02:13 +0530

    Working with Textual data

--- python-learning/01_variables_and_datatypes/variables.py
+++ python-learning/01_variables_and_datatypes/variables.py
@@ -3,3 +3,19 @@ message = "Hello World"
 print(message)
 
 
+# how to count the length of the string
+print(len(message)) 
+
+
+# how to access a specific character in the string
+print(message[10]) 
+
+
+# how to access a specific character in the string using negative indexing
+print(message[-11])
+
+
+# how to access a range of characters in the string
+print(message[0:5]) # it print Hello
+print(message[6:12]) # it print World
+# here the first index is inclusive and the second index is exclusive
